7e53d20807f2bd976560683ce66d96e0506cf89b
Karina Lenivtseva <solovyevakarina@yandex.ru>
2022-06-28 19:20:33 +0300

    Task 1 created

---
+++ Task1/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Write("Введите первое число: ");
+int number_1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int number_2 = Convert.ToInt32(Console.ReadLine());
+if (number_1 > number_2)
+    Console.WriteLine("Первое число больше второго");
+else if (number_1 < number_2)
+    Console.WriteLine( "Первое число меньше второго" ); 
