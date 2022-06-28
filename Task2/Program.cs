2aadd5eadbf42379eb89075e912dd665fede2bff
Karina Lenivtseva <solovyevakarina@yandex.ru>
2022-06-28 20:14:36 +0300

    Task 2 finished

---
+++ Task2/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите первое число: ");
+int number_1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int number_2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите третье число: ");
+int number_3 = Convert.ToInt32(Console.ReadLine());
+int max = number_1;
+if(number_1 > max ) max = number_1;
+if(number_2 > max ) max = number_2;
+if(number_3 > max ) max = number_3;
+Console.Write("max = ");
+Console.WriteLine(max);
